befc97eeb6ece7af6564b2af3c45bf87e6cb5d78
Matteo Fortini <matteo.fortini@gmail.com>
2024-04-04 19:07:35 +0200

    Primi dati e script di partenza

---
+++ srcdata/scripts/filter_cup_json.sh
@@ -0,0 +1,1 @@
+duckdb -c "COPY (SELECT DISTINCT (CUPCIG.*) FROM read_json_auto('../rawdata/cup_json.json') CUPCIG JOIN read_json_auto('../rawdata/candidature_comuni_finanziate.json') CAND ON CUPCIG.CUP=CAND.codice_cup) TO '../data/cupcig_candidature_comuni_finanziate.json'"
---
+++ srcdata/scripts/get_candidature_comuni.sh
@@ -0,0 +1,1 @@
+wget --output-document=../rawdata/candidature_comuni_finanziate.json https://raw.githubusercontent.com/teamdigitale/padigitale2026-opendata/main/data/candidature_comuni_finanziate.json
