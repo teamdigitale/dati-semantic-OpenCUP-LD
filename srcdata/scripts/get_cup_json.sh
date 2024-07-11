12414924321d0a069706bef2106a7a2460cef8f8
Matteo Fortini <matteo.fortini@gmail.com>
2024-07-11 14:31:46 +0200

    scripts: fixes and typos

--- srcdata/scripts/get_cup_json.sh
+++ srcdata/scripts/get_cup_json.sh
@@ -1,2 +1,2 @@
 wget --output-document=../rawdata/cup_json.zip https://dati.anticorruzione.it/opendata/download/dataset/cup/filesystem/cup_json.zip
-unzip -d ../rawdata/ ../rawdata/cup_json.zip
+unzip -o -d ../rawdata/ ../rawdata/cup_json.zip
